3a9516d9824fe1415bf1ceb5b12868117dc01a6c
unknown <matthieu.lu.pro@gmail.com>
2021-05-13 16:51:31 +0200

    Init timeline manager

---
+++ Assets/Resources/CutScenes/TimelineManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineManager : MonoBehaviour
+{
+    private bool fix = false;
+    public Animator playerAnimator;
+    public RuntimeAnimatorController playerAnim;
+    public PlayableDirector director;
+
+    void OnEnable()
+    {
+        playerAnim = playerAnimator.runtimeAnimatorController;
+        playerAnimator.runtimeAnimatorController = null;         
+    }
+
+    void Update()
+    {
+        if(director.state != PlayState.Playing && !fix) {
+            fix = true;
+            playerAnimator.runtimeAnimatorController = playerAnim;
+        }
+    }
+}
